a3229c185db8f4afffd77baa7f05d50284e00175
Bruno Oliveira <bruno.oliveira.trabalho@hotmail.com>
2017-07-14 13:14:04 -0300

    Heroku

--- symfony-of-destruction/src/ApiDestructionBundle/Controller/DefaultController.php
+++ symfony-of-destruction/src/ApiDestructionBundle/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace ApiDestructionBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,5 +15,13 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('ApiDestructionBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/amorzinho")
+     */
+    public function amorzinhoAction()
+    {
+        return new Response("Te amo muito mais sempre <3", 200);
     }
 }
